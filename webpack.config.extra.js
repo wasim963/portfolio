3763da9ef83347dfd4bba8c5b51acd2fd3bf93e9
Wasim Akram <wacim4u@gmail.com>
2023-11-22 01:06:38 +0530

    header css updated

--- webpack.config.extra.js
+++ webpack.config.extra.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 exports.alias = {
+    // ui components
     'components': path.resolve( __dirname, 'src/js/components' ),
-    'hoc': path.resolve( __dirname, 'src/js/hoc' )
+    'hoc': path.resolve( __dirname, 'src/js/hoc' ),
+
+    // scss globals
+    'scss-globals': path.resolve( __dirname, 'src/scss/globals/index.scss' )
 }
