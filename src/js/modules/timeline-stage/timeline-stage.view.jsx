21745359c88c5602557a618dd32a8086363dddee
Wasim Akram <wacim4u@gmail.com>
2024-04-14 03:24:05 +0530

    experience page - timeline stages impl

--- src/js/modules/timeline-stage/timeline-stage.view.jsx
+++ src/js/modules/timeline-stage/timeline-stage.view.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-export const timelineStage = () => {
+export const timelineStage = ( props ) => {
+  const { primaryClassName, isFirst, isLast, side } = props;
+
+  const widgetClassName = `ui-timeline-stage ${primaryClassName}`;
+  const sideLineClass = `ui-timeline-stage__center ui-timeline-stage--${side}`
+
   return (
-    <div className='ui-timeline-stage' >timeline-stage.view</div>
+    <div className={widgetClassName} >
+      {
+        !isFirst && 
+        <div className="ui-timeline-stage__top"></div>
+      }
+      <div className={sideLineClass}>
+        <div className={"ui-timeline-stage__center__line"}></div>
+        <div className={"ui-timeline-stage__center__circle"}></div>
+      </div>
+      {
+        !isLast &&
+        <div className="ui-timeline-stage__bottom"></div>
+      }
+    </div>
   )
 }
 
