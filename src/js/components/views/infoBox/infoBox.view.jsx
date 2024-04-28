130a0bc93d7ae27569f35e8526f6c0cc1255feb3
Wasim Akram <wacim4u@gmail.com>
2024-04-28 22:17:18 +0530

    timeline widget impl

--- src/js/components/views/infoBox/infoBox.view.jsx
+++ src/js/components/views/infoBox/infoBox.view.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+// local dependencies
+import { Icon } from '@modules/icon-bank';
 
 export const infoBox = ( props ) => {
     const widgetClassName = `ui-info-box ${props?.primaryClassName}`
 
     return (
-        <div className={widgetClassName} >infoBox.view</div>
+        <div className={widgetClassName} >
+            <div className="ui-info-box__header">
+                <div className="ui-info-box__header__period">{props.startDate} - {props.endDate ? props?.endDate : 'Present'}</div>
+                <div className="ui-info-box__header__role">{ props.roleType }</div>
+            </div>
+            <div className="ui-info-box__body">
+                <div className="ui-info-box__body__icon">
+                    <Icon name={'moon'} width={72} height={72} />
+                </div>
+                <div className="ui-info-box__body__details">
+                    <h3>{props?.designation}</h3>
+                    <h4>{props.company}</h4>
+                    <p>{ props?.location }</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
