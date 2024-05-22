a76fe8f82c060858d984e3f710ee8205f2950bdf
Wasim Akram <wacim4u@gmail.com>
2024-05-22 20:56:17 +0530

    SASS THEME feature added

--- src/js/components/views/skills/skills.view.jsx
+++ src/js/components/views/skills/skills.view.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // local dependencies
 import { isMobile, isTablet } from '@utils/responsiveViewportHook/responsiveViewportHook.util';
+import { THEME_CONTEXT } from "@utils/themeProviderHook";
 
 // local views to render
 import { Card } from "@modules/card";
 import { Icon } from "@modules/icon-bank";
 
 export const SkillsView = ( props ) => {
-  const { title, description, primaryClassName, skills } = props;
+    const { title, description, primaryClassName, skills } = props;
+    const { theme } = useContext(THEME_CONTEXT);
 
   // main class name
   const widgetClassName = `ui-skills ${primaryClassName}`;
@@ -16,8 +18,11 @@ export const SkillsView = ( props ) => {
   // viewport width and height
   const _isMobile = isMobile();
   const _isTablet = isTablet();
-  const iconWidth = _isMobile ? 64 : ( _isTablet ? 80 : 96 );
-  const iconHeight = _isMobile ? 64 : ( _isTablet ? 80 : 96 );
+  const iconWidth = _isMobile ? 64 : ( _isTablet ? 96 : 112 );
+  const iconHeight = _isMobile ? 64 : (_isTablet ? 96 : 112);
+  
+   console.log('THEME', THEME)
+
 
   return (
     <Card 
@@ -31,7 +36,12 @@ export const SkillsView = ( props ) => {
                     skills?.map( ( skill, index ) => {
                         return(
                             <span key={index} className="ui-skills__body__skills__skill">
-                                <Icon name={skill} width={iconWidth} height={iconHeight} />
+                                <Icon
+                                    name={skill}
+                                    width={iconWidth}
+                                    height={iconHeight}
+                                    fill={theme === 'dark' ? THEME.light : THEME.dark}
+                                />
                             </span>
                         )
                     } )
@@ -50,5 +60,5 @@ SkillsView.defaultProps = {
   title: "Skills",
   description: "My bucket of expertise",
   primaryClassName: '',
-  skills: [ 'react', 'javascript', 'html', 'css', 'scss', 'webpack', 'node', 'typescript', 'git', 'npm' ]
+  skills: [ 'react', 'next', 'javascript', 'html', 'css', 'scss', 'webpack', 'node', 'typescript', 'git', 'npm' ]
 };
